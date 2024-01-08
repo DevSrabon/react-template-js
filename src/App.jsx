c8f2923504b0e26bbc4d17ff1cc634b96b3d592a
devsrabon <et.srabon@gmail.com>
2024-01-08 23:09:18 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-    const [count, setCount] = useState(0);
-    console.log(objectdfdf);
+    const [count, setCount] = useStat(0);
+
 
     return (
         <>
