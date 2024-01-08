0a41b68c1298d940a58c7a96f377252cc4a6876e
devsrabon <et.srabon@gmail.com>
2024-01-08 23:20:58 +0600

    dklfjl

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-    const [count, setCount] = useStat(0);
-dsfdfsdfdf
+    const [count, setCount] = useState(0);
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button onClick={() => setCount((count) => coun + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
